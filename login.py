aae5d94fe803e991ebde7748d2293403862c8abc
flaviosalesv <flavio.salesv@gmail.com>
2026-09-15 10:44:24 -0300

    feat: connect login to menu and add error message

--- login.py
+++ login.py
@@ -18,9 +18,12 @@ password_entry.pack()
 
 def button_entry():
         if user_entry.get() == 'usuario' and password_entry.get() == '000':
+            for widget in window.winfo_children():
+                widget.destroy()
+
             menu_choices(window)
         else:
-            print('ERROR')
+            ctk.CTkLabel(window, text='Usuário ou senha incorretos!').pack()
 
 
 ctk.CTkButton(window, text='Login', command=button_entry).pack()
